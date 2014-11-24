fc180228d182579b9637170361f04c434824a5f7
slinbody <slinbody@gmail.com>
2014-11-24 13:55:09 +0800

    Create Switch1.py

---
+++ Switch1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''模擬switch的功能 '''
+score = int(input('請輸入分數：'))
+level = score // 10
+print("test")
+{
+    10 : lambda: print('Perfect'),
+    9  : lambda: print('A'),
+    8  : lambda: print('B'),
+    7  : lambda: print('C'),
+    6  : lambda: print('D')
+}.get(level, lambda: print('E'))()
+
