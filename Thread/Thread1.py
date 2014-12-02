a7ba99b4876fe300c61c854bf46cb079d237b53f
slinbody <slinbody@gmail.com>
2014-12-02 16:11:54 +0800

    Create Thread1.py

---
+++ Thread/Thread1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import _thread as thread, time
+def counter(myId, count):
+for i in range(count):
+time.sleep(1)
+print('[%s] => %s' % (myId, i)) # function run in threads
+for i in range(5):
+thread.start_new_thread(counter, (i, 5)) # spawn 5 threads
+# each thread loops 5 times
+time.sleep(6)
+print('Main thread exiting.')
