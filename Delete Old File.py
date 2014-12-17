46c951b41b6b8ad74a7fbdcae3157365e74b87f6
slinbody <slinbody@gmail.com>
2014-12-17 10:37:20 +0800

    Update Delete Old File.py

--- Delete Old File.py
+++ Delete Old File.py
@@ -1,6 +1,9 @@
 import os
 import datetime
+dir_to_search = '/Your/Path'
 for dirpath,dirnames,filenames in os.walk(dir_to_search):
     for file in filenames:
         curpath=os.path.join(dirpath,file)
-        print(curpath)
+        file_modified = datetime.datetime.fromtimestamp(os.path.getmtime(curpath))
+        if datetime.datetime.now() - file_modified > datetime.timedelta(hours=72):
+          os.remove(curpath)
