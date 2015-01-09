628dbcc6a4c7eda8a815851bdeb753c126909bd6
slinbody <slinbody@gmail.com>
2015-01-09 18:14:24 +0800

    Update Download.py

--- Download.py
+++ Download.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 import os
+from urllib.parse   import quote
 from urllib.request import urlopen
 filename = '5.jpg'
 
-remoteaddr = b'http://upload.wikimedia.org/wikipedia/zh/f/fe/5.jpg'
+remoteaddr = 'http://upload.wikimedia.org/wikipedia/zh/f/fe/'+quote('三重-金城五')+'5.jpg'
 
 remotefile = urlopen(remoteaddr)
 localfile = open(filename, 'wb')
