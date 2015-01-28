3f932bd1753a1098092d35d58458a771cca084be
slinbody <slinbody@gmail.com>
2015-01-28 13:20:26 +0800

    closure

---
+++ Re_Example_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import re
+
+def build_match_and_apply_functions(pattern, search, replace):
+    def matches_rule(word):    
+        return re.search(pattern, word)
+    def apply_rule(word):    
+        return re.sub(search, replace, word)
+    return (matches_rule, apply_rule)    
+
+patterns = \ 
+  (
+    ('[sxz]$',           '$',  'es'),
+    ('[^aeioudgkprt]h$', '$',  'es'),
+    ('(qu|[^aeiou])y$',  'y$', 'ies'),
+    ('$',                '$',  's')    
+  )
+
+rules = [build_match_and_apply_functions(pattern, search, replace)  
+         for (pattern, search, replace) in patterns]
+
+print(rules[0][1]('box'))
