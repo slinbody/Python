d62aa57dfdaf9ec105b754604907e73f457bde54
slinbody <slinbody@gmail.com>
2016-07-30 15:07:52 +0800

    Update Rail-time.py

--- Rail-time.py
+++ Rail-time.py
@@ -7,10 +7,14 @@ import datetime
 
 fromstation=str(1008)
 tostation=str(1031)
+
 now = datetime.datetime.now()
 today = now.strftime("%Y/%m/%d")
 fromtime= now.strftime("%H%M")
 #print(fromtime)
+
+if datetime.datetime.now() >  datetime.datetime.combine(datetime.datetime.today(),datetime.time(14, 0)):
+        (fromstation,tostation) = (tostation,fromstation) #若超過下午兩點，出發與目的交換
 
 rs = requests.get('http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchtype=0&searchdate='+today+'&fromcity=0&tocity=0&fromstation='+fromstation+'&tostation='+tostation+'&trainclass=2&timetype=1&fromtime='+fromtime+'&totime=2359')
 
@@ -28,4 +32,5 @@ for row in rows:
         cols = row.find_all('td')
         x = [element.text.strip() for element in cols]
         print(x[0],x[3],x[4],x[5],x[6])
+
 
