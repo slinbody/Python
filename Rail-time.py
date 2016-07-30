c8c9eb1e576b3f5b2a9da3ddb79c61a4b9cb0f3c
slinbody <slinbody@gmail.com>
2016-07-30 19:53:31 +0800

    Update Rail-time.py

--- Rail-time.py
+++ Rail-time.py
@@ -3,20 +3,21 @@ import requests
 from bs4 import BeautifulSoup as bs
 import datetime
 
-"""Station編號: 萬華1009  台北1008  松山1007  汐科1031"""
+Stationnum={1009:'萬華',1008:'台北',1007:'松山',1031:'汐科'}#車站編號
 
 fromstation=str(1008)
 tostation=str(1031)
 
-now = datetime.datetime.now()        #現在時間
-today = now.strftime("%Y/%m/%d")    
-fromtime= now.strftime("%H%M")       #搜尋啟始時間
+now = datetime.datetime.now()
+today = now.strftime("%Y/%m/%d")
+fromtime= now.strftime("%H%M")
 totime= (datetime.datetime.today() + datetime.timedelta(hours=2)).strftime("%H%M")
 if fromtime > totime:
         totime='2359'
+#print(fromtime)
 
 if datetime.datetime.now() >  datetime.datetime.combine(datetime.datetime.today(),datetime.time(14, 0)):
-        (fromstation,tostation) = (tostation,fromstation)
+        (fromstation,tostation)= (tostation,fromstation)
 
 rs = requests.get('http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchtype=0&searchdate='+today+'&fromcity=0&tocity=0&fromstation='+fromstation+'&tostation='+tostation+'&trainclass=2&timetype=1&fromtime='+fromtime+'&totime='+totime)
 
@@ -24,8 +25,9 @@ rs = requests.get('http://twtraffic.tra.gov.tw/twrail/SearchResult.aspx?searchty
 soup = bs(rs.content,"html5lib")
 #print(soup.prettify())
 
-print("萬華1009  台北1008  松山1007  汐科1031")
-print("This is from "+ fromstation+" to "+tostation+' on '+today+'\n')
+#print("萬華1009  台北1008  松山1007  汐科1031")
+print(today)
+print(Stationnum[int(fromstation)]+" 往 "+Stationnum[int(tostation)]+'\n')
 
 table = soup.find('table',attrs={'id':'ResultGridView'})
 table_body = table.find('tbody')
