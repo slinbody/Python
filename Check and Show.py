1a136c03455736d94788ff9c1a8c8598bb9a65c1
slinbody <slinbody@gmail.com>
2016-08-17 09:28:10 +0800

    Create Check and Show.py

---
+++ Check and Show.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+'''
+檢查檔案是否有被修改，有則讀出並顯示
+'''
+import os
+import subprocess
+from time import sleep
+filename = '/etc/config/cmd_result'
+if not os.path.exists(filename):
+    os.system('touch '+filename)
+statinfo = str(os.stat(filename)).split(',')[8]
+oldinfo = ''
+while True:
+    subprocess.call('clear',shell=True)
+    if statinfo != oldinfo:
+        with open(filename,'r') as f:
+            print(f.read())
+    sleep(2)
