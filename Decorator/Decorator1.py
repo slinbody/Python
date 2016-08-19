0c7dcc6e2213d966f84ba3525991fecd5b135249
slinbody <slinbody@gmail.com>
2016-08-19 10:32:10 +0800

    Create Decorator1.py

---
+++ Decorator/Decorator1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+def Big(func):
+    def Mid(*args,**kwargs):
+        print 'function name :'+func.__name__
+        print 'x = ',args[0],'y = ',args[1]
+        return func(*args,**kwargs)
+    return Mid
+
+
+@Big
+def add(x,y):
+    print"x+y=",x+y
+
+add(4,6)
