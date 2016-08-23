68fe68b0c96eef3f084bfdca2b87082a5623e2bf
slinbody <slinbody@gmail.com>
2016-08-23 16:17:57 +0800

    Create sub.py

---
+++ Redis/TEST/sub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2.7
+'''
+讀取訊息
+'''
+import redis
+import time
+
+#r = redis.StrictRedis(host='localhost', port=6379, db=0)           #用網路連
+r = redis.StrictRedis(unix_socket_path='/tmp/netmgr-redis.sock')    #用socket連
+p = r.pubsub()
+p.subscribe('test')
+
+while True:
+    message = p.get_message()
+    if message:
+        print "Subscriber: %s" % message['data']
+    time.sleep(1)
