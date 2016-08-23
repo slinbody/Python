1fd3444824575055b3653822721c37fa67519d62
slinbody <slinbody@gmail.com>
2016-08-23 16:16:19 +0800

    Create pub.py

---
+++ Redis/TEST/pub.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2.7
+import redis
+import time
+
+#queue = redis.StrictRedis(host='localhost', port=6379, db=0)            # 用網路連
+queue = redis.StrictRedis(unix_socket_path='/tmp/netmgr-redis.sock')     # 用socket連
+channel = queue.pubsub()
+
+for i in range(10):
+    queue.publish("test", i)
+    time.sleep(0.5)
