37df7efcca82f3c934ac7b37353020d7528d072f
slinbody <slinbody@gmail.com>
2016-08-23 16:18:53 +0800

    Update sub.py

--- Redis/TEST/sub.py
+++ Redis/TEST/sub.py
@@ -6,7 +6,7 @@ import redis
 import time
 
 #r = redis.StrictRedis(host='localhost', port=6379, db=0)           #用網路連
-r = redis.StrictRedis(unix_socket_path='/tmp/netmgr-redis.sock')    #用socket連
+r = redis.StrictRedis(unix_socket_path='/tmp/redis.sock')    #用socket連
 p = r.pubsub()
 p.subscribe('test')
 
