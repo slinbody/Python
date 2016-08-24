c56fdfa0c47a7398f88c95ba26efcd95d108cf67
slinbody <slinbody@gmail.com>
2016-08-24 22:00:02 +0800

    Create Progressbar.py

---
+++ Progressbar.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+進度條
+'''
+from time import sleep
+import sys
+
+for i in range(21):
+    sys.stdout.write('\r')
+    # the exact output you're looking for:
+    #sys.stdout.write("[%-20s] %d%%" % ('='*i, 5*i))
+    #sys.stdout.write("%d%%" % (5*i))
+    sys.stdout.flush()
+    sleep(0.25)
