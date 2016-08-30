8c1a44eb9a36e27607f0e06535ca78175a7439d2
slinbody <slinbody@gmail.com>
2016-08-30 15:41:36 +0800

    Update ConfigParser-Example.py

--- ConfigParser-Example.py
+++ ConfigParser-Example.py
@@ -2,22 +2,21 @@
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
-filename = '/home/pi/myproject/test.ini'
-fd = open(filename,'r+')
-config.readfp(fd)
 
-print config.get('My Section','foodir')
-print config.get('My Section','dir1')
+with open('/etc/network/interface','r+') as fd:
+  config.readfp(fd)
 
-if not config.has_section('Section2'):
-    config.add_section('Section2')
-config.set('Section1', 'an_int', '09915')
-config.set('Section2', 'a_bool', 'true')
-config.set('Section2', 'a_float', '3.1415')
-config.set('Section1', 'baz', 'funny')
-config.set('Section1', 'bar', 'Python')
-config.set('Section1', 'foo', '%(bar)s is %(baz)s!')
+  print config.get('My Section','foodir')
+  print config.get('My Section','dir1')
 
-fd.seek(0)
-config.write(fd)
-fd.close()
+  if not config.has_section('Section2'):
+      config.add_section('Section2')
+  config.set('Section1', 'an_int', '09915')
+  config.set('Section2', 'a_bool', 'true')
+  config.set('Section2', 'a_float', '3.1415')
+  config.set('Section1', 'baz', 'funny')
+  config.set('Section1', 'bar', 'Python')
+  config.set('Section1', 'foo', '%(bar)s is %(baz)s!')
+  fd.seek(0)  #從頭開始寫
+  config.write(fd)
+
