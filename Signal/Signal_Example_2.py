4d678203a1d633e046db436ea6aa310a1f978181
slinbody <slinbody@gmail.com>
2016-08-31 23:08:36 +0800

    Create Signal_Example_2.py

---
+++ Signal/Signal_Example_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+'''
+10秒內要收到 'kill -USR1 PID'
+沒收到就停止程式 
+使用Global變數的方法來寫
+'''
+import signal
+import os
+import time
+i=10
+def receive_signal(signum, stack):
+    global i
+    print 'Received:', signum
+    i = 10
+
+signal.signal(signal.SIGUSR1, receive_signal)
+print 'My PID is:', os.getpid()
+
+while i>0:
+    print 'i: ',i
+    i = i - 1
+    time.sleep(3)
