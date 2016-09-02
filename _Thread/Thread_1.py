bdad415b0f1417456c5e99e5d1a59f7412cf6939
slinbody <slinbody@gmail.com>
2016-09-02 10:13:31 +0800

    Update Thread_1.py

--- _Thread/Thread_1.py
+++ _Thread/Thread_1.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 import _thread as thread, time
-def counter(myId, count):
+  fd = open('/tmp/thread-test','a+')
   for i in range(count):
     time.sleep(1)
-    print('[%s] => %s' % (myId, i)) # function run in threads
+    fd.write('thread [%s] => 第%s次\n' % (myId,i))
+  fd.close()
+
 
 for i in range(5):
   thread.start_new_thread(counter, (i, 5)) # spawn 5 threads
 # each thread loops 5 times
-time.sleep(6)
+time.sleep(6)  # 若無此行，就結束，thread跟著結束
 print('Main thread exiting.')
