f434fc5e7140bd82dd780d1b3813f28cd6f2ee64
slinbody <slinbody@gmail.com>
2016-09-26 12:08:59 +0800

    Update Switch_2.py

--- Switch_2.py
+++ Switch_2.py
@@ -1,13 +1,15 @@
 '''
 Example of switch case
 '''
-def x1():
+def x1(a,b):
         for i in sys.argv[:]:
                 print i
 
-def y1():
+def y1(a,b):
         for i in reversed(sys.argv[:]):
                 print i
+def z1(a,b):
+        print a,b
 
 if __name__ == '__main__':
         case_fun = {
@@ -15,4 +17,5 @@ if __name__ == '__main__':
                 'y': y1,
                 'z': z1
         }
-        case_fun[sys.argv[1]]()
+        case_fun[sys.argv[1]](sys.argv[2],sys.argv[3])
+
