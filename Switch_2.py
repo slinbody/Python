7aa287a73478aded82b28514cae0a8a593252eff
slinbody <slinbody@gmail.com>
2016-09-26 14:27:53 +0800

    Update Switch_2.py

--- Switch_2.py
+++ Switch_2.py
@@ -1,6 +1,7 @@
 '''
 Example of switch case
 '''
+import sys
 def x1(a,b):
         for i in sys.argv[:]:
                 print i
@@ -10,12 +11,18 @@ def y1(a,b):
                 print i
 def z1(a,b):
         print a,b
+def default():
+        print "HIHI"
 
 if __name__ == '__main__':
         case_fun = {
                 'x': x1,
                 'y': y1,
-                'z': z1
+                'z': z1,
         }
-        case_fun[sys.argv[1]](sys.argv[2],sys.argv[3])
+        try:
+                case_fun[sys.argv[1]](sys.argv[2],sys.argv[3])
+        except KeyError:
+                default()
+
 
