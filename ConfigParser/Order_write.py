4e55125c87bdf6c1bd13f223962baedcdbaa273d
slinbody <slinbody@gmail.com>
2016-10-27 22:08:14 +0800

    Update Order_write.py

--- ConfigParser/Order_write.py
+++ ConfigParser/Order_write.py
@@ -11,3 +11,12 @@ for i in order_list:
     config_two.set('global', i, config_one.get('global',i))
 with open('/tmp/test.ini','w+') as fd:
     config_two.write(fd)
+
+    
+### you can create a file "/tmp/test.ini" as following: ###
+[global]
+b = 2
+a = 1
+c = 3
+e = 5
+d = 4
