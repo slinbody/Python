4326c0355e1df386ab7d53f7769493ae2a850fb0
slinbody <slinbody@gmail.com>
2016-12-29 10:26:12 +0800

    Create block_nonblock.py

---
+++ threading/block_nonblock.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# You can find explanation at http://www.bogotobogo.com/python/Multithread/python_multithreading_Event_Objects_between_Threads.php
+import threading
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',)
+
+def wait_for_event(e):
+    logging.debug('wait_for_event starting')
+    event_is_set = e.wait() # wait until return True
+    logging.debug('event set: %s', event_is_set)
+
+def wait_for_event_timeout(e, t):
+    while not e.isSet():
+        logging.debug('wait_for_event_timeout starting')
+        event_is_set = e.wait(t) # wait t seconds, if not True, return False
+        logging.debug('event set: %s', event_is_set)
+        if event_is_set:
+            logging.debug('processing event')
+        else:
+            logging.debug('doing other things')
+
+if __name__ == '__main__':
+    e = threading.Event()
+    t1 = threading.Thread(name='blocking',
+                      target=wait_for_event,
+                      args=(e,))
+    t1.start() # threading start
+
+    t2 = threading.Thread(name='non-blocking',
+                      target=wait_for_event_timeout,
+                      args=(e, 2))
+    t2.start() # threading start
+
+    logging.debug('Waiting before calling Event.set()')
+    time.sleep(3)
+    e.set() # set to True
+    logging.debug('Event is set')
