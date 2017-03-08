87e3d4c6b7c255c8688449e010fc6510d49366fe
slinbody <slinbody@gmail.com>
2017-03-08 10:09:45 +0800

    Create Colorful-2.py

---
+++ color/Colorful-2.py
@@ -0,0 +1,7 @@
+
+# 1 ~ 100會出現顏色變化
+from __future__ import print_function
+for i in range(1,1000):
+    sys.stdout.write('\033[38;5;'+str(i)+'m'+str(i)+'\033[0m  ')
+    if (i % 10) == 0:
+         print ''
