9a817f18a12970cf0a0ed5c1ac0735c261f4159e
slinbody <slinbody@gmail.com>
2017-05-02 22:50:40 +0800

    Create show_today_stock.py

---
+++ Stock/show_today_stock.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+'''
+抓證交所的個股當天收盤資料
+'''
+import urllib2
+from lxml import etree
+import datetime
+
+def get_today():
+    year = str(int('20'+datetime.datetime.now().strftime("%y")) - 1911)
+    today = datetime.datetime.now().strftime("/%m/%d")
+    return year+today
+
+def show_message(stock_id):
+    title=['日期', '成交股數', '成交金額', '開盤價', '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數']
+
+    url = "http://www.tse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAYMAIN.php"
+    data = "download:&query_year=2017&query_month=5&CO_ID="+stock_id+"&query_button='查詢'"
+    request = urllib2.Request(url, data)
+    f = urllib2.urlopen(url, data)
+#    print f.read()
+
+    today = get_today()
+    html =  etree.HTML(f.read())
+    tr_nodes = html.xpath("//table[@align='center']/tbody/tr")
+    for tr_element in tr_nodes:
+#        print tr_element[0].text
+        if today == tr_element[0].text:
+            for index, td_element in enumerate(tr_element):
+                print "{}{} {}".format(title[index], ":", td_element.text)
+
+if __name__ == '__main__':
+    show_message('2412')
