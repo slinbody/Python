07f1936e4c0ed01359cbb1064e0ac6d44abbe0f0
slinbody <slinbody@gmail.com>
2017-05-27 01:09:31 +0800

    Create Count.py

---
+++ Count_Menu/Count.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+result = {}
+
+with open('/tmp/menu') as fd:
+    for line in fd:
+        line = line.replace('\n','')
+        line = line.split('+')
+        for x in line:
+            x = x.replace(" ","").decode('UTF-8')
+            if x not in result.keys():
+#                print x
+                result[x] = 1
+            else:
+                result[x] = result[x] + 1
+    for (k,v) in result.items():
+        print k,v
