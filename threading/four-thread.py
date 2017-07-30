0ae37f225bb1e2c6d32221a531486e8fb71bd7d0
slinbody <slinbody@gmail.com>
2017-07-30 21:39:02 +0800

    Update four-thread.py

--- threading/four-thread.py
+++ threading/four-thread.py
@@ -20,3 +20,18 @@ thread.start()
 threading.Thread(target=action, args=(2,)).start() 
 
 _thread.start_new_thread(action, (2,))
+
+##  The way to manipulation more than one arguments
+list_a = range(10)
+dic_b = {}
+def action(i):
+    b[i] = i*2
+
+threads = []    
+for i in list_a:
+    thread = threading.Thread(target=action, args=(i,))
+    thread.start()
+    threads.append(thread)
+    
+for thread in threads: # Here is to wait all finished
+    thread.join()
