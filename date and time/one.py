a2ff32bec2338b94659caf90c3d1e29cb5d0f82d
slinbody <slinbody@gmail.com>
2017-10-31 15:35:31 +0800

    Update one.py

--- date and time/one.py
+++ date and time/one.py
@@ -6,4 +6,31 @@ month = 11
 
 for i in range(1,31):
     x = datetime.datetime(year, month, i)
-    print(x.strftime("%Y/%m/%d"))
+    if x.weekday()  < 5:
+        print(x.strftime("%Y/%m/%d %A"))
+        
+## result
+2017/11/01 Wednesday
+2017/11/02 Thursday
+2017/11/03 Friday
+2017/11/06 Monday
+2017/11/07 Tuesday
+2017/11/08 Wednesday
+2017/11/09 Thursday
+2017/11/10 Friday
+2017/11/13 Monday
+2017/11/14 Tuesday
+2017/11/15 Wednesday
+2017/11/16 Thursday
+2017/11/17 Friday
+2017/11/20 Monday
+2017/11/21 Tuesday
+2017/11/22 Wednesday
+2017/11/23 Thursday
+2017/11/24 Friday
+2017/11/27 Monday
+2017/11/28 Tuesday
+2017/11/29 Wednesday
+2017/11/30 Thursday
+
+
