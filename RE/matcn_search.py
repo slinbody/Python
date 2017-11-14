fc4b771bf945ba1e6e6c8cef85565b7eaf2ef8f5
slinbody <slinbody@gmail.com>
2017-11-14 08:51:24 +0800

    Update matcn_search.py

--- RE/matcn_search.py
+++ RE/matcn_search.py
@@ -3,7 +3,7 @@ import re
 ip = '11111000101010000000000100000000'
 m1 =re.match('1+',ip)  # search from begining of string, must match at begining
 print m1.group()
-m2 = re.search('1+',ip)  # search throught string to find the first match position
+m2 = re.search('1+',ip)  # search throught string to find the first match position and stop
 print m2.group()
 m3 = re.findall('1+', ip)  # search throght string and print all match string
 print m3
