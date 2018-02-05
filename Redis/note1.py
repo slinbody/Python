bc2b7b90cdc3da424e7641e9207d28f4a9a0f537
slinbody <slinbody@gmail.com>
2018-02-05 21:50:03 +0800

    Create note1.py

---
+++ Redis/note1.py
@@ -0,0 +1,18 @@
+# about dict of dict in redis
+import redis
+
+redis_db = redis.StrictRedis(host='xxx.aaa.com', port=6379, db=0)
+user = {"Name":"Pradeep", "Company":"SCTL", "Address":"Mumbai", "Location":"RCP"}
+
+redis_db.hmset("TEST1", user) 
+
+redis_db.hgetall("TEST1")
+# {'Company': 'SCTL', 'Address': 'Mumbai', 'Location': 'RCP', 'Name': 'Pradeep'}
+
+redis_db.hset("TEST1", "Name", "Pradeep11")
+
+redis_db.hgetall("TEST1")
+# {'Company': 'SCTL', 'Address': 'Mumbai', 'Location': 'RCP', 'Name': 'Pradeep111'}
+
+redis_db.keys()
+redis_db.delete("TEST1")
