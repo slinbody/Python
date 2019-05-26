af21e923dfb26ab4c45141077fdd231b8278113a
slinbody <slinbody@gmail.com>
2019-05-26 09:06:01 +0800

    Update and rename get_ip_address.py to get_ip_by_interface.py

--- Net_Manage/get_ip_by_interface.py
+++ Net_Manage/get_ip_by_interface.py
@@ -9,7 +9,7 @@ def get_ip_address(ifname):
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
+        struct.pack('256s', ifname.encode('utf8')[:15])
     )[20:24])
 
 get_ip_address('eth0')  # '192.168.0.110'
