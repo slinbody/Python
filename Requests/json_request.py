0ef918825b6bfd2836a2464bc52336f18d3a4eb5
slinbody <slinbody@gmail.com>
2020-04-07 17:00:31 +0800

    Create json_request.py

---
+++ Requests/json_request.py
@@ -0,0 +1,21 @@
+# https://www.programcreek.com/python/example/6251/requests.post
+def getTicket():
+    # put the ip address or dns of your apic-em controller in this url
+    url = "https://" + controller + "/api/v1/ticket"
+
+    #the username and password to access the APIC-EM Controller
+    payload = {"username":"usernae","password":"password"}
+
+    #Content type must be included in the header
+    header = {"content-type": "application/json"}
+
+    #Performs a POST on the specified url to get the service ticket
+    response= requests.post(url,data=json.dumps(payload), headers=header, verify=False)
+
+    #convert response to json format
+    r_json=response.json()
+
+    #parse the json to get the service ticket
+    ticket = r_json["response"]["serviceTicket"]
+
+    return ticket 
